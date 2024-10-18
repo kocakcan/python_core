b59e83a5a7124467ada486901ca79218da14ab40
Can Kocak1 <can_kocak1@epam.com>
2024-10-18 10:09:01 +0300

    added rest of the L4: Loops

--- code/loops.py
+++ code/loops.py
@@ -44,3 +44,50 @@ while x < 5:
 else:
     print("Loop was stopped at:")
     print(x)
+
+for i in range(5):
+    print(i)
+
+# for operator is designed to use sequences and collections.
+# That's why we can use the built-in function range
+# that generates a sequence of numbers and the loop iterates
+# with each item from this sequence.
+# Using for in Python simplifies the code compared to using while
+# We don't need to initiate counters before the loop
+# and don't care about incrementing counters.
+x = int(input("Input integer number: "))
+is_prime = True
+for div in range(2, x):
+    if not x % div:
+        is_prime = False
+        break
+
+if is_prime:
+    print("Prime")
+else:
+    print("Not prime")
+
+# We can also use continue, break, and else operators as we did in while loops.
+x = int(input("Input integer number: "))
+
+for div in range(2, x):
+    if not x % div:
+        print("Not prime")
+        break
+else:
+    print("Prime")
+# Because the else statement works only if the break operator is not executed
+# it helps to avoid redundant variables and makes code simpler.
+
+# Iterators are quite important in Python as for statement can be used with any iterable object.
+# In other words, with any object that implements the iterator protocol.
+
+# "Iterable" - an object capable of returning its members one at a time.
+# Examples of iterables include all sequence types (such as list, str, and tuple)
+# and some non sequence types like dict, file objects, and any objects of any
+# classes we define with an __iter__() method or with a __getitem__() method that
+# implements Sequence semantics.
+
+# "Iterator" - an object representing a stream of data. Repeated calls to the iterator's
+# __next__() method (or passing it to the built-in function next() return successive items
+# in the stream. When no more data are available, a StopIteration exception is raised instead.

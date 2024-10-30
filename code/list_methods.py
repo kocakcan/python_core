231605d50de85af0964f5f2eff4ae31317f9c285
cankocak <can_kocak1@epam.com>
2024-10-30 16:15:57 +0300

    added the rest of the methods

--- code/list_methods.py
+++ code/list_methods.py
@@ -81,3 +81,13 @@ print(numbers)
 # Equivalent to a[:].
 nums = numbers.copy()
 print(nums)
+
+fruits = ["orange", "apple", "pear", "banana", "kiwi", "apple", "banana"]
+print(fruits.count("apple"))
+print(fruits.index("banana"))
+fruits.reverse()
+print(fruits)
+fruits.append("grape")
+print(fruits)
+fruits.sort()
+print(fruits)

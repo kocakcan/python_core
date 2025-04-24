4ec03e48c34b5b6af4eed0490d9065dc4039b7ff
cankocak <can_kocak1@epam.com>
2025-04-24 19:22:36 +0300

    added M5. Modules and Packages

---
+++ code/m5_modules_and_packages/l0.py
@@ -0,0 +1,8 @@
+"""
+- Modules and packages refer to modular programming. According to this
+  concept, large tasks are broken into smaller manageable subtasks or
+  modules. These small pieces can be combined to create complex
+  applications. Splitting large applications into smaller parts brins the
+  following benefits: Readability, maintainability, reusability,
+  manageability.
+"""

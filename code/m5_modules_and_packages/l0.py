f9729b6baf0f9807894e9c238195ab435f934b56
cankocak <can_kocak1@epam.com>
2025-04-25 17:23:23 +0300

    fixed a typo

--- code/m5_modules_and_packages/l0.py
+++ code/m5_modules_and_packages/l0.py
@@ -2,7 +2,7 @@
 - Modules and packages refer to modular programming. According to this
   concept, large tasks are broken into smaller manageable subtasks or
   modules. These small pieces can be combined to create complex
-  applications. Splitting large applications into smaller parts brins the
+  applications. Splitting large applications into smaller parts brings the
   following benefits: Readability, maintainability, reusability,
   manageability.
 """

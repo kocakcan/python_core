c202c3c2f5f1bdb64e31819ec3a62218c011f3d5
cankocak <can_kocak1@epam.com>
2025-04-25 19:13:25 +0300

    started L0

---
+++ code/m6_oop/l1.py
@@ -0,0 +1,14 @@
+# L1: OOP Basics
+
+"""
+A programming paradigm is a way to classify programming languages based on
+their features.
+
+Object-oriented programming (OOP) is the programming paradigm that allows using
+object-oriented analysis and design (OOAD) to model software design around
+objects rather than logic or functions.
+
+Objects are real-world entities that you can feel, manipulate and sense. For
+example, an object can represent a person with the following properties: age,
+name, gender, and behaviours like eating, sleeping, walking, etc.
+"""

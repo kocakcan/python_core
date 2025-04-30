8ee1c629755042e0984aafeccb5287863a959ac2
cankocak <can_kocak1@epam.com>
2025-04-30 18:16:49 +0300

    continue with L3

--- code/m6_oop/l4.py
+++ code/m6_oop/l4.py
@@ -1,1 +1,9 @@
 # L4: Magic Methods
+
+"""
+In Python, there is a function called dir(), which lists all the attributes of
+the given object, and if you use dir() function for your object or object
+class, you will discover attributes like __class__, __doc__, __str__ etc. These
+are called magic methods because they are implicitly called when some action
+happens.
+"""

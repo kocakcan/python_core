80b0270d51ecd21d7274ffd69901cdde2ae11c78
cankocak <can_kocak1@epam.com>
2025-12-08 16:56:11 +0300

    fixed the logic error 🦁

--- code/binsearch.py
+++ code/binsearch.py
@@ -1,9 +1,9 @@
 def binsearch(n: list[int], x: int) -> int:
     low = 0
-    high = len(n)
+    high = len(n) - 1
 
-    while low < high:
-        mid = int((low + high) / 2)
+    while low <= high:
+        mid = (low + high) // 2
         if n[mid] < x:
             low = mid + 1
         elif n[mid] > x:
@@ -16,7 +16,9 @@ def binsearch(n: list[int], x: int) -> int:
 
 def main():
     n = list(range(0, 10))
-    print(f"Result: {binsearch(sorted(n), 5)}")
+    print(f"Result: {binsearch(sorted(n), 0)}")
+    # Negative
+    print(f"Result: {binsearch(sorted(n), 10)}")
 
 
 if __name__ == "__main__":
